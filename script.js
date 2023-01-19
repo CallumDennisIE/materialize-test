425b9aa573003e9f78cb6aa33d7906e9ec2a4922
Callum Dennis <callumdennisie@gmail.com>
2023-01-19 21:38:40 +0000

    feat: add chart.js graphs page

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 M.AutoInit();
+
+var xValues = ["Expenses", "Emergency", "Investment", "Learning", "Fun"];
+var yValues = [55, 49, 44, 24, 15];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+
+new Chart("spending", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Spending Breakdown 2022 - 2023"
+    }
+  }
+});
+
+var xValues = ["Expenses", "Emergency", "Investment", "Learning", "Fun"];
+var yValues = [55, 49, 22, 31, 22];
+var barColors = [
+    "#b91d47",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+];
+
+new Chart("bucket", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Bucket Spending Breakdown 2022 - 2023"
+    }
+  }
+});
